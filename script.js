16eb21cc12bbb787cefd05d778bd57f3620b5a42
spacexjedi <58277328+spacexjedi@users.noreply.github.com>
2020-08-18 16:12:57 -0300

    fb to vulkans

---
+++ script.js
@@ -0,0 +1,9 @@
+function popalertcadastrar(){
+            var aviso = document.getElementById('pop-alert-cadastrar')
+            aviso.innerHTML = 'Espero que você esteja legalizado na galáxia.'
+        }
+
+        function popalertentrar(){
+            var aviso = document.getElementById('pop-alert-entrar')
+            aviso.innerHTML = 'Espero que você esteja legalizado na galáxia.'
+        }
